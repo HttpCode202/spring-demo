f679d406b9acb54290b538a345fe84ace9e3cff1
HttpCode202 <neant69neant89@gmail.com>
2024-08-15 22:41:15 +0200

    :sparkles: end webflux (#3)

--- webflux/src/main/java/fr/HttpCode202/demo/flux/FirstFluxService.java
+++ webflux/src/main/java/fr/HttpCode202/demo/flux/FirstFluxService.java
@@ -7,12 +7,39 @@ import reactor.core.publisher.Mono;
 @Service
 public class FirstFluxService {
 
+    private Flux<String> numberfluxOneToFive() {
+        return Flux.just("One", "Two", "Three", "Four");
+    }
+
+    private Flux<String> numberfluxSixToTen() {
+        return Flux.just("Five", "Six", "Seven", "Eight", "Nine", "Ten");
+    }
+
     public Flux<String> numberflux() {
-        return Flux.just("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten");
+        return numberfluxOneToFive().concatWith(numberfluxSixToTen());
     }
 
     public Flux<String> numberfluxWithError() {
         return numberflux().concatWith(Mono.error(new IllegalArgumentException("my error")));
+    }
+
+    public Flux<String> numberfluxWithSleep() {
+        return numberfluxOneToFive().concatWith(
+                Mono.create(callback -> {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException exception) {
+                        throw new RuntimeException(exception);
+                    }
+                    callback.success();
+                })).concatWith(numberfluxSixToTen());
+    }
+
+    public Flux<String> numberfluxWithZip() {
+        return numberfluxOneToFive().zipWith(
+                numberfluxSixToTen(),
+                (String flux1, String flux2) -> String.format("%s%s", flux1, flux2)
+        );
     }
 
 }
